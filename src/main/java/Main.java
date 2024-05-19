d97280c7c58f8c4e1b93a8a54e0fabba81826378
dmitryhatsayuk <dmitry.hatsayuk@gmail.com>
2024-05-19 23:45:16 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,82 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        List<Employee> list2 = parseXML("data.xml");
+        writeString("data.json", listToJson(list));
+        writeString("data2.json", listToJson(list2));
+    }
+
+    private static List<Employee> parseXML(String s) throws IOException, SAXException, ParserConfigurationException {
+        List<Employee> employeeList = new ArrayList<Employee>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(s));
+        Node root = doc.getDocumentElement();
+        NodeList staff = doc.getElementsByTagName("employee");
+        for (int i = 0; i < staff.getLength(); i++) {
+            Node node = staff.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                Employee employee = new Employee();
+                employee.id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                employee.firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                employee.lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                employee.country = element.getElementsByTagName("country").item(0).getTextContent();
+                employee.age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+                employeeList.add(employee);
+            }
+        }
+        return employeeList;
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+
+        List<Employee> list;
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader).withMappingStrategy(strategy).build();
+            list = csv.parse();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        Gson gson = new Gson();
+        return gson.toJson(list, listType);
+    }
+
+    private static void writeString(String fileName, String target) throws FileNotFoundException {
+        try (FileWriter jd = new FileWriter(fileName)) {
+            jd.write(target);
+        } catch (IOException e) {
+            throw new FileNotFoundException();
+        }
+    }
+}
